7759d1b0ee57adc5b91feff552e980aa70467b05
Rnveer007 <rvs523474@gmail.com>
2025-01-24 12:33:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className='flex items-center justify-evenly h-[826px] bg-teal-800'>
         <div className=''>
-          <h1 className='text-black font-bold text-5xl w-[500px] leading-16'>Take Your <span className='text-red-800'>Knowledge</span> to the next Level</h1>
+          <h1 className='text-white font-bold text-5xl w-[500px] leading-16'>Take Your <span className='text-yellow-400'>Knowledge</span> to the next Level</h1>
           <button className='border-2 px-10 py-1 bg-black text-white mt-8 cursor-pointer'>Start</button>
         </div>
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,65 @@
 import React, { useState } from 'react'
 
 function Header() {
-  const [ showUserBox , setShowUserBox ] = useState (false)
+  const [ showUserBox , setShowUserBox ] = useState (false);
+  const [ userInput , setUserInput ] = useState("");
+  const [savedUserInput, SetSavedUserInput ] = useState("");
+  const [addUserValue , setAddUserValue] = useState(false);
+  const [succesfullAlert , setSuccesfullAlert ] = useState (false)
+  // const [showUserCreatedMsg , setShowUserCreatedMsg ] = useState('')
+
   
   function showCreateUserBTn(){
-    setShowUserBox(!showUserBox)
+    setShowUserBox(true)
   
   }
+
+  function quitCreatUserbox(){
+    setShowUserBox(false)
+  }
+
+  function addUser(){
+     setShowUserBox(false)
+     setAddUserValue (true)
+
+     SetSavedUserInput(userInput)
+  }
+
+   function addUserAndShowAlert (){
+   addUser()
+   showSuccesfullAlert()
+   }
+
+  function showSuccesfullAlert(){
+    setTimeout(() => {
+      setSuccesfullAlert(true)
+    }, 2000);
+
+  }
+
 
   return (
     <>
       <div className='h-full bg-black relative'>
-      <div className='flex justify-evenly bg-black h-full py-12'>
-        <h1 className='text-white text-2xl'>Quiz Pop</h1>
-        <button onClick={showCreateUserBTn} className='text-white text-xl border-2 px-3 py-1 cursor-pointer'>Create User</button>
+      <div className='flex justify-evenly bg-black h-full py-12 items-center'>
+        <h1 className='text-white text-2xl '>Quiz Pop</h1>
+        <div><button onClick={showCreateUserBTn} className={`text-white text-xl border-2 px-3 py-1 cursor-pointer ${ addUserValue ? 'hidden' : '' }`}>Create User</button>
+
+        <h1 className={`text-white text-xl px-3 py-2 cursor-pointer capitalize ${ addUserValue ? '' : 'hidden'} `}>{savedUserInput}</h1>
+        </div>
       </div>
+
+      <div className={`font-bold w-[300px]  bg-green-600 text-center py-3 absolute right-160 top-50  ${ addUserValue ? '' : 'hidden' }  ${ succesfullAlert ? 'hidden' : '' }`}> <h1>User Created Succesfully</h1></div>
 
       <div id='hideUserbox' className={`bg-white w-[600px] h-[250px] px-10 absolute right-120 ${showUserBox ? '' : 'hidden'}`}>
         <h3 className='text-2xl my-8'>Create Username</h3>
-        <input type="text" name="" id="" placeholder='Enter Your Name' className='border-2 w-[400px] py-1 pl-6' />
+        <input type="text" name="" id="" placeholder='Enter Your Name' value={userInput} onChange={(e) => {setUserInput(e.target.value)}} className='border-2 w-[400px] py-1 pl-6' />
+
         <div className='flex justify-between my-10 w-[250px]'>
-        <button className='bg-black text-white px-10 py-1 cursor-pointer'>Create</button>
-        <button className='bg-red-800 text-white px-10 py-1 cursor-pointer'>Quit</button>
+        <button onClick={addUserAndShowAlert} className='bg-black text-white px-10 py-1 cursor-pointer'>Create</button>
+        <button onClick={quitCreatUserbox} className='bg-red-800 text-white px-10 py-1 cursor-pointer'>Quit</button>
         </div>
       </div>
-      
       </div>
     </>
   )
