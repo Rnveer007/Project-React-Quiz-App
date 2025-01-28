3ee41da6ad1c879add14d032feaa95e8548b407d
Rnveer007 <rvs523474@gmail.com>
2025-01-28 16:44:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,42 @@ import './App.css'
 import logo from './assets/img.png';
 
 function App({ userAvailable, setUserAvailable }) {
-  function showAlert() {      
-    setUserAvailable(true)
+  const [showAlert, setShowAlert] = useState(false)
+  function showRedAlert() {
+    setShowAlert (true)
+
     setTimeout(() => {
-      setUserAvailable(false)
+      setShowAlert(false)
     }, 2000);
   }
 
   return (
     <>
-      <div className='flex items-center justify-evenly h-[826px] bg-teal-800 '>
+      <div className='flex items-center justify-evenly h-[826px] bg-teal-800 hidden'>
 
         <div className=''>
           <h1 className='text-white font-bold text-5xl w-[500px] leading-16'>Take Your <span className='text-yellow-500'>Knowledge</span> to the next Level</h1>
-          <button onClick={showAlert} className='border-2 px-10 py-1 bg-black text-white mt-8 cursor-pointer'>Start</button>
+          <button onClick={showRedAlert} className='border-2 px-10 py-1 bg-black text-white mt-8 cursor-pointer'>Start</button>
         </div>
         {/* Create User Alert */}
-        <div className={`text-white font-bold w-[300px] bg-red-600 text-center py-3 absolute right-180 top-50 ${userAvailable ? "" : "hidden"}`} >
+        <div className={`text-white font-bold w-[300px] bg-red-600 text-center py-3 absolute right-180 top-50 ${  showAlert ? "" : "hidden" }`} >
           <h1> User not found </h1>
         </div>
 
         <div><img src={logo} alt="" /></div>
       </div>
+
+
+    <div className='secondPage bg-yellow-800 h-screen flex justify-center'>
+      <div className='mt-12 h-full '>
+        <h1 className='text-white text-4xl text-center my-4 pb-3'>Welcome to Quize App</h1>
+        <p className='text-center text-white my-4 text-xl'>Select Topic</p>
+        <button className='block w-[500px] my-4 py-3 bg-white cursor-pointer border-2'>MUSIC</button>
+        <button className='block w-[500px] my-4 py-3 bg-white cursor-pointer border-2'>MODERN ART</button>
+        <button className='block w-[500px] my-4 py-3 bg-white cursor-pointer border-2'>CODING </button>
+        <button className='block border-2 bg-black text-white px-10 m-auto cursor-pointer py-2'>Start Quiz</button>
+      </div>
+    </div>
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,48 +4,43 @@ import './App.css'
 
 function Header({ userAvailable, setUserAvailable }) {
   const [showUserBox, setShowUserBox] = useState(false);
-  const [userInput, setUserInput] = useState("");
-  const [addUserValue, setAddUserValue] = useState(false);
   const [succesfullAlert, setSuccesfullAlert] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [showUserName, setUserName] = useState(false);
   const [latestUser, setLatestUser] = useState(null);
 
   const [users, SetUsers] = useState(
-    localStorage.getItem('user') !== null ? JSON.parse(localStorage.getItem("user")) : []
+    localStorage.getItem('userData') !== null ? JSON.parse(localStorage.getItem("userData")) : []
   )
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(users))
+    localStorage.setItem("userData", JSON.stringify(users))
   }, [users]);
 
-  function showCreateUserBTn() {
+  function showCreateUserBox() {
     setShowUserBox(true);
   }
 
-  function quitCreatUserbox() {
+  function hideCreateUserbox() {
     setShowUserBox(false);
     setUserInput("")
   }
 
-  function addUserAndShowAlert() {
+
+  function hideUserCreateBoxAndShowSuccessAlert() {
     if (userInput === "") {
-      alert("Username cannot be empty.");
+      alert("Username cannot be empty.")
       return;
     }
 
-    setShowUserBox(false);
-    setAddUserValue(true);
-
-
-    
     const userObj = { id: Date.now(), name: userInput };
-    SetUsers([...users, userObj])
+    SetUsers([...users, userObj]);
     setLatestUser(userObj)
 
-    setAddUserValue(true)
-    setUserInput("");
-
+    setShowUserBox(false);
     setSuccesfullAlert(true);
-    setUserAvailable(true);// Set userAvailable to true
+    setUserName(true)
+
     setTimeout(() => {
       setSuccesfullAlert(false);
     }, 2000);
@@ -56,30 +51,47 @@ function Header({ userAvailable, setUserAvailable }) {
   return (
     <>
       <div className="h-full bg-black relative">
-        <div className="flex justify-evenly bg-black h-full py-12 items-center">
+        <div className="flex justify-evenly bg-black h-full py-12 items-center hidden">
           <h1 className="text-white text-2xl">Quiz Pop</h1>
           <div>
             <button
-              onClick={showCreateUserBTn}
-              className={`text-white text-xl border-2 px-3 py-1 cursor-pointer ${addUserValue ? "hidden" : ""
-                }`}
+              onClick={showCreateUserBox}
+              className={`text-white text-xl border-2 px-3 py-1 cursor-pointer ${showUserName ? "hidden" : ""}`}
             >
               Create User
             </button>
 
             <h1
-              className={`text-white text-xl px-3 py-2 cursor-pointer capitalize ${addUserValue ? "" : "hidden"
-                }`}
+              className={`text-white text-xl px-3 py-2 cursor-pointer capitalize ${showUserName ? "" : "hidden"}`}
             >
               <span>{latestUser?.name}</span>
             </h1>
           </div>
         </div>
 
+
+
+        <div className="flex justify-evenly bg-black h-full py-12 items-center">
+          <h1 className="text-white text-2xl">Quiz Pop</h1>
+
+          <ul className="flex text-white">
+            <li className="mx-4">Home</li>
+            <li className="mx-4">Quiz</li>
+            <li className="mx-4">Score</li>
+          </ul>
+
+          <h1
+            className={`text-white text-xl px-3 py-2 cursor-pointer capitalize`}
+          >
+            <span>{latestUser?.name}</span>
+          </h1>
+        </div>
+
+
+
         {/* Success Alert */}
         <div
-          className={`font-bold w-[300px] bg-green-600 text-center py-3 absolute right-170 top-50  ${succesfullAlert ? "" : "hidden"
-            }`}
+          className={`font-bold w-[300px] bg-green-600 text-center py-3 absolute right-170 top-50 ${succesfullAlert ? "" : "hidden"}`}
         >
           <h1>User Created Successfully</h1>
         </div>
@@ -87,8 +99,7 @@ function Header({ userAvailable, setUserAvailable }) {
         {/* User Input */}
         <div
           id="hideUserbox"
-          className={`bg-white w-[600px] h-[250px] px-10 absolute right-120  ${showUserBox ? "" : "hidden"
-            }`}
+          className={`bg-white w-[600px] h-[250px] px-10 absolute right-120 ${showUserBox ? "" : "hidden"} `}
         >
           <h3 className="text-2xl my-8">Create Username</h3>
           <input
@@ -100,17 +111,18 @@ function Header({ userAvailable, setUserAvailable }) {
           />
 
           <div className="flex justify-between my-10 w-[250px]">
-            <button onClick={addUserAndShowAlert}
+            <button onClick={hideUserCreateBoxAndShowSuccessAlert}
               className="bg-black text-white px-10 py-1 cursor-pointer">
               Create
             </button>
-            <button onClick={quitCreatUserbox}
+            <button onClick={hideCreateUserbox}
               className="bg-red-800 text-white px-10 py-1 cursor-pointer">
               Quit
             </button>
           </div>
         </div>
       </div>
+
     </>
   );
 }
