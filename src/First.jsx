2c16d281e6b299aabe38c38ca3ef76c83e462a83
Rnveer007 <rvs523474@gmail.com>
2025-02-01 21:55:17 +0530

    updated

--- src/First.jsx
+++ src/First.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import Header from './header';
+import Header from './Header';
 import App from './App';
 import QuiestionShow from './QuiestionShow';
 
